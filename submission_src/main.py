244b57736a3d001283335dfb02c001af1c2c7b16
Michael Schlauch <mike@drivendata.org>
2022-04-20 12:20:18 -0400

    Add underscore etc

--- submission_src/main.py
+++ submission_src/main.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import pandas as pd
 import typer
 
-ROOT_DIRECTORY = Path("/codeexecution")
-PREDICTIONS_DIRECTORY = ROOT_DIRECTORY / "predictions"
-SUBMISSION_DIRECTORY = ROOT_DIRECTORY / "submission"
+ROOT_DIRECTORY = Path("/code_execution")
+PREDICTION_FILE = ROOT_DIRECTORY / "submission" / "submission.csv"
+DATA_DIRECTORY = ROOT_DIRECTORY / "data"
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     reference_ids = ['R00A', 'R00B', 'R00C', 'R01A', 'R01B', 'R02A', 'R02B']
     scores = [1., .9, .8, 1., .9, 1., .9]
     submission = pd.DataFrame({"query_id": query_ids, "reference_id": reference_ids, "score": scores})
-    submission.to_csv(SUBMISSION_DIRECTORY / "submission.csv", index=False)
+    submission.to_csv(PREDICTION_FILE, index=False)
 
 
 if __name__ == "__main__":
