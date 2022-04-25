3b3be756031475af8b45746ff40059f867d25922
Jay Qi <jayqi@users.noreply.github.com>
2022-04-25 13:43:52 -0400

    Use predict method

--- submission_quickstart/main.py
+++ submission_quickstart/main.py
@@ -8,6 +8,12 @@ DATA_DIRECTORY = ROOT_DIRECTORY / "data"
 OUTPUT_FILE = ROOT_DIRECTORY / "submission.csv"
 
 
+def predict(query_image_id, database_image_ids):
+    # Predict first 20 images, excluding query_image
+    result_images = database_image_ids[database_image_ids != query_image_id][:20].tolist()
+    scores = [0.5] * len(result_images)
+    return result_images, scores
+
 def main():
     scenarios_df = pd.read_csv(DATA_DIRECTORY / "query_scenarios.csv")
 
@@ -23,15 +29,16 @@ def main():
             query_image_id = query_row.query_image_id
             database_image_ids = database_df["database_image_id"].values
 
-            # Predict first 20 images, excluding query_image
-            result_images = database_image_ids[database_image_ids != query_image_id][:20]
+            ### Prediction happens here ######
+            result_images, scores = predict(query_image_id, database_image_ids)
+            ##################################
 
-            for pred_image_id in result_images:
+            for pred_image_id, score in zip(result_images, scores):
                 predictions.append(
                     {
                         "query_id": query_id,
                         "database_image_id": pred_image_id,
-                        "score": 0.5,
+                        "score": score,
                     }
                 )
 
